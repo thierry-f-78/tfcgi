940aef75f9b0641af00a743c3996939d5daa0c20
Thierry FOURNIER <tfournier@arpalert.org>
2019-05-26 12:07:46 +0200

    MINOR: Set licence

--- tfcgi.c
+++ tfcgi.c
@@ -1,3 +1,13 @@
+/* 
+ * tfcgi.c: FastCGI API for starting threads
+ *
+ * Copyright 2008-2009 Thierry Fournier (http://cv.arpalert.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
 #include <errno.h>
 #define NO_FCGI_DEFINES
 #include <fcgi_stdio.h>
